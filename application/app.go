9e2fcde8681ef591c293fc677d0cbaa380ecb021
zishan044 <winchesterfelix007@gmail.com>
2024-01-29 21:56:23 +0600

    implemented graceful shutdown

--- application/app.go
+++ application/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -31,11 +32,32 @@ func (a *App) Start(ctx context.Context) error {
 		fmt.Errorf("Failed to connect to redis %w", err)
 	}
 
+	defer func() {
+		err := a.rdb.Close()
+		if err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
 	fmt.Println("Starting server")
 
-	err = server.ListenAndServe()
-	if err != nil {
-		fmt.Errorf("Failed to start server: %w", err)
+	errch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			errch <- fmt.Errorf("Failed to start server: %w", err)
+		}
+	}()
+
+	select {
+	case err = <-errch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
 	}
 
 	return nil
--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/zishan044/orders-api/application"
 )
 
 func main() {
 	app := application.New()
-	if err := app.Start(context.TODO()); err != nil {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	if err := app.Start(ctx); err != nil {
 		fmt.Errorf("failed to start app: %w", err)
 	}
 }
