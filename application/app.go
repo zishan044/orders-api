ccf34680f87f9709802b239d47e4f44b1d0a7dc9
zishan044 <winchesterfelix007@gmail.com>
2024-01-29 20:40:02 +0600

    redis integration

--- application/app.go
+++ application/app.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	return &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 }
 
@@ -21,8 +25,18 @@ func (a *App) Start(ctx context.Context) error {
 		Addr:    ":3000",
 		Handler: a.router,
 	}
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Errorf("Start error: %w", err)
+
+	err := a.rdb.Ping(ctx).Err()
+	if err != nil {
+		fmt.Errorf("Failed to connect to redis %w", err)
 	}
+
+	fmt.Println("Starting server")
+
+	err = server.ListenAndServe()
+	if err != nil {
+		fmt.Errorf("Failed to start server: %w", err)
+	}
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/zishan044/orders-api
 
 go 1.21.6
 
-require github.com/go-chi/chi v1.5.5
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/redis/go-redis/v9 v9.4.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
